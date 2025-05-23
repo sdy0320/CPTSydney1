ce5afd71af71cfe2a400c9000ddc44517585c1c2
sdy0320 <sydney.khang26@ycdsbk12.ca>
2025-05-23 09:39:20 -0400

    Added more con.printlns for fun

--- CPTSydney.java
+++ CPTSydney.java
@@ -4,8 +4,25 @@ public class CPTSydney{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
+		con.println("Dumplings");
+		con.println("        _________________");
+		con.println("       |||||||||||||||||||  ");
+		con.println("       |||||||||||||||||||          ________________    ");
+		con.println("       [*   ()     ()   *]        _|____'|____|'__ _|_   ");
+		con.println("        * ''    ㅠ   ''  *       |||||||||||||||||||||");
+		con.println("         *_____________*         ||*   __     __   *||  ");
+		con.println("       **     *___*     **       |[*   ()     ()   *]|   ");
+		con.println("     *  |_______________|  *     ||* ''    o   ''  *|| ");
+		con.println("     *__|      <3       |__*     |||*_____________*|||     ");
+		con.println("     *  |---------------|  *     |**     *___*     **|     ");
 
-		
+		con.println("        |      ___      |       *  |               |  *  ");
+		con.println("        |      | |      |       *__|               |__*     ");
+		con.println("        |      | |      |       (__)               (__) ");
+	
+	
+
+				
 
 		
 		
