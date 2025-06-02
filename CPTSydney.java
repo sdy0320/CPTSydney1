8b55acc467afd41e1c99cf61a600de2c0977323a
sdy0320 <sydney.khang26@ycdsbk12.ca>
2025-06-02 11:29:57 -0400

    Added new imports for key events. Moved the drawings for mainscreen into a method and added new methods choosing theme, and picking randomized word. Fixed errors in main menu and theme method.
    Added new imports for key events, such as p for play, q for quit. Moved the drawings for mainscreen into a method called mainmenu. Called both methods into main program. In tools; Added a method for playing the game, and created methods for choosing a random word, choosing theme user wants, and started method for users guesses. Fixed up method PlayGame and called all methods within mainmenu

--- CPTSydney.java
+++ CPTSydney.java
@@ -2,45 +2,38 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.Font;
+import java.awt.event.*;
+import javax.swing.*;
 import arc.*;
 public class CPTSydney{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-		int intX = 400;
-		int intY = 200;
-		BufferedImage imgLogo = con.loadImage("HangmanLogo.png");
-		Font fntTest = con.loadFont("JourneyWhiskers-Regular.ttf", 40);
-		while(intX > 0){
-			// set the background
-			con.setDrawColor(Color.BLACK);
-			con.fillRect(0,0,700,700);
-			// Start drawing stuff on that background
-			con.setDrawColor(Color.WHITE);
-			con.fillOval(100, 100, 50, 200);
-			// Setting a predefined color
-			con.setDrawColor(Color.RED);
-			con.fillOval(400, 10, 100, 75);
-			// Setting a custom Red Blue Green rgb color 0-255
-			con.setDrawColor(new Color(3, 100, 230));
-			con.fillRoundRect(300, intY, 200, 200, 50, 50);
-			intY = intY + 3;
-			con.drawImage(imgLogo, intX, 0);
-			intX = intX - 1;
-			con.setDrawFont(fntTest);
-			con.setDrawColor(new Color(200, 30, 30));
-			con.drawString("Mmmmm doughnuts", 0,0);
-			
-			// repainting ensures that all of the drawing commands above appear
-			con.repaint();
-			// Waiting for 33ms before looping around again
-			// to show the drawing on the screen
-			// before looping back around
-			con.sleep(33);	
-
+		JFrame frame = new JFrame("Hangmanzz");
+		frame.addKeyListener(CPTSydneytools.PlayonPKeyListener(con));
+		frame.addKeyListener(CPTSydneytools.QuitonQKeyListener(con));
+		//BufferedImage imgLogo = con.loadImage("HangmanLogo.png");
+		// set the background colour
+		CPTSydneytools.drawMainMenu(con);
+		CPTSydneytools.QuitonQKeyListener(con);
+		// Setting a predefined color
+		//con.setDrawColor(Color.RED);
+		//con.fillOval(400, 10, 100, 75);
+	
+		//con.drawImage(imgLogo, 0, 0);
+	
+		con.setDrawColor(new Color(200, 30, 30));
+		con.drawString("Hangman Menu", 75,0);
+		
+		// repainting ensures that all of the drawing commands above appear
+		con.repaint();
+		// Waiting for 33ms before looping around again
+		// to show the drawing on the screen
+		// before looping back around
+		con.sleep(33);
 		
 		
 	}	
 	
-	}
+
 }
--- CPTSydneytools.java
+++ CPTSydneytools.java
@@ -18,14 +18,15 @@ public class CPTSydneytools{
 				con.fillRect(50, intY, 200, 50);
 				intY = intY+70;
 			}
-		
+			CPTSydneytools.PlayGame(con);
 		}
 		
-		public static String PlayGame(Console con, String strWord){
+		public static void PlayGame(Console con){
 			String strUserName;
 			String strTest = "";
 			String strTheme = "";
-			int intUserChoice;
+			String strWord;
+			String strUserChoice;
 			//Getting user's name
 			con.println("Enter User Name: ");
 			//Printing Themes
@@ -38,18 +39,18 @@ public class CPTSydneytools{
 			themeNames.close();
 			//Using ThemeChosen method to get the theme the user wanted.
 			con.println("Choose a theme: ");
-			intUserChoice = con.readInt();
-			strTheme = CPTSydneytools.Themechosen(intUserChoice);
+			strUserChoice = con.readLine();
+			strTheme = CPTSydneytools.Themechosen(strUserChoice);
+			strWord = CPTSydneytools.ChooseWord(strTheme);
+			//WORKS THEME & WORD
+			//con.println(strTheme);
+			//con.println(strWord);
 			
-			TextInputFile currentTheme = new TextInputFile(strTheme);
-			
-			
-			return strTest;
 		}
 		
 		
 		
-		public static String Themechosen(int intUserChoice){
+		public static String Themechosen(String strUserChoice){
 			TextInputFile themes = new TextInputFile("themes.txt");
 			String strThemeName = "";
 			int intCount  = 0;
@@ -57,19 +58,82 @@ public class CPTSydneytools{
 			String strChosenTheme = "";
 			while(themes.eof() == false){
 				strThemeName = themes.readLine();
-				strThemes[intCount] = strThemeName;
-				intCount = intCount+1;
-			}
-			
-			for(intCount = 0; intCount <= intUserChoice; intCount++){
-				if(intUserChoice == intCount){
-					strChosenTheme = strThemes[intCount];
+				if(strThemeName.equalsIgnoreCase(strUserChoice)){
+					strChosenTheme = strThemeName;
 				}
 			}
 			
 			return strChosenTheme;
 		}	
-	
+		
+		public static String ChooseWord(String strTheme){
+			TextInputFile currentTheme = new TextInputFile(strTheme);
+			//make the code that will choose a random word from the text. Will need to use bubble sort and put the data from the text file into a 2D array
+			int intCount = 0;
+			String strWord = "";
+			int intRand;
+			String strThemeWords [][];
+			strThemeWords = new String [10][2];
+			while (currentTheme.eof() == false){
+				strWord = currentTheme.readLine();
+				strThemeWords[intCount][0] = strWord;
+				intRand = (int)(Math.random()*100+1);
+				strThemeWords[intCount][1] = intRand + "";
+				intCount = intCount+1;
+			}
+			
+			//Bubble sorting
+			int intCount2;
+			String strWordTemp;
+			String strRandNum;
+			
+			for(intCount2 = 0; intCount2 < 10-1; intCount2++){
+				for (intCount = 0; intCount < 10-1; intCount++){
+					//Convert the people's score to integer and compare
+					if(Integer.parseInt(strThemeWords[intCount][1]) > Integer.parseInt(strThemeWords[intCount+1][1])){
+						//Swap here
+						//swap name
+						strWordTemp = strThemeWords[intCount][0];
+						strThemeWords[intCount][0] = strThemeWords[intCount+1][0];
+						strThemeWords[intCount+1][0] = strWordTemp;
+						//swap critics
+						strRandNum = strThemeWords[intCount][1];
+						strThemeWords[intCount][1] = strThemeWords[intCount+1][1];
+						strThemeWords[intCount+1][1] = strRandNum;
+				
+					}
+				}
+			}
+			strWord = strThemeWords[0][0];
+			return strWord;
+		}
+		
+		
+		public static String UserGuess(String strUserChoice){
+			TextInputFile themes = new TextInputFile("themes.txt");
+			String strThemeName = "";
+			int intCount  = 0;
+			//String strThemes [] = new String [5];
+			String strChosenTheme = "";
+			while(themes.eof() == false){
+				strThemeName = themes.readLine();
+				if(strThemeName.equalsIgnoreCase(strUserChoice)){
+					strChosenTheme = strThemeName;
+				}
+				
+				//strThemes[intCount] = strThemeName;
+				//intCount = intCount+1;
+			}
+			
+			//for(intCount = 0; intCount <= intUserChoice; intCount++){
+				//if(intUserChoice == intCount){
+					//strChosenTheme = strThemes[intCount];
+				//}
+			//}
+			
+			return strChosenTheme;
+		}
+		
 		public static KeyListener QuitonQKeyListener(Console con){
 			return new KeyAdapter(){
 				public void keyPressed(KeyEvent e) {
