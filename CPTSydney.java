fde2bd7c6b9a1cfa8ffdf9cac80306d8d3287d2b
sdy0320 <sydney.khang26@ycdsbk12.ca>
2025-06-03 12:15:48 -0400

    Changed font and started drawing themes and main menu background. Worked on getchar for key shortcuts in game, created a new method to simplify playgame method.
    - Started drawing other pages, and fixing bugs with images. reformatting code to simplify.

--- CPTSydney.java
+++ CPTSydney.java
@@ -2,25 +2,26 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.Font;
-import java.awt.event.*;
-import javax.swing.*;
+
+
 import arc.*;
 public class CPTSydney{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-		JFrame frame = new JFrame("Hangmanzz");
-		frame.addKeyListener(CPTSydneytools.PlayonPKeyListener(con));
-		frame.addKeyListener(CPTSydneytools.QuitonQKeyListener(con));
+
 		//BufferedImage imgLogo = con.loadImage("HangmanLogo.png");
 		// set the background colour
+	
 		CPTSydneytools.drawMainMenu(con);
-		CPTSydneytools.QuitonQKeyListener(con);
+		
+	
 		// Setting a predefined color
 		//con.setDrawColor(Color.RED);
 		//con.fillOval(400, 10, 100, 75);
 	
 		//con.drawImage(imgLogo, 0, 0);
+	
 	
 		con.setDrawColor(new Color(200, 30, 30));
 		con.drawString("Hangman Menu", 75,0);
--- CPTSydneytools.java
+++ CPTSydneytools.java
@@ -2,23 +2,113 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.Font;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.KeyListener;
+
 import arc.*;
 public class CPTSydneytools{
 	//methods 1
 		public static void drawMainMenu(Console con){
+			boolean blnwaiting = true;
+		
+		
 			con.setDrawColor(Color.WHITE);
 			con.fillRect(0,0,300,800);
 			int intCount = 0;
 			int intY = 90;
 			// Start drawing stuff on that background
 			for(intCount = 0; intCount<4; intCount++){
-				con.setDrawColor(Color.GRAY);
+				con.setDrawColor(new Color(166, 227, 245));
 				con.fillRect(50, intY, 200, 50);
 				intY = intY+70;
+	
 			}
-			CPTSydneytools.PlayGame(con);
+			
+			Font fntMenu = con.loadFont("AmericanTypewriter.ttc", 25);
+			con.setDrawFont(fntMenu);
+			con.setDrawColor(new Color (32,91,110));
+			con.drawString("Play", 125,95);
+			con.drawString("Leaderboard", 75,165);
+			con.drawString("Rules", 116,235);
+			con.drawString("Quit", 120,305);
+	
+			
+			while(blnwaiting == true){
+				char chrControl = con.getChar();
+			
+				if (chrControl == 'p' || chrControl == 'P'){
+					blnwaiting = false;
+				
+					CPTSydneytools.UserNameInfo(con);
+					CPTSydneytools.Themes(con);
+					CPTSydneytools.PlayGame(con);
+				}
+			}
+			
+			
+		
+			
+		
+			//CPTSydneytools.PlayGame(con);
+		}
+		
+		
+		public static String UserNameInfo(Console con){
+			String strUsername = "";
+			con.setDrawColor(Color.WHITE);
+			con.fillRect(0,0,300,800);
+			
+			con.setDrawColor(new Color(166, 227, 245));
+			con.fillRect(50, 200, 200, 50);
+			
+			con.setDrawColor(new Color (32,91,110));
+			con.drawString("Enter name: ", 80,95);
+			strUsername = con.readLine();
+			con.drawString(strUsername, 100,200);
+			con.sleep(33);
+			return strUsername;
+		}
+		
+		
+		
+		public static String Themes(Console con){
+			con.clear();
+			String strTheme = "";
+			String strUserChoice = "";
+			con.setDrawColor(Color.BLACK);
+			con.fillRect(0,0,300,800);
+			BufferedImage imgthemes = con.loadImage("newthemebackground.png");
+			con.drawImage(imgthemes, 0, -30);
+			
+			
+			
+			// Start drawing stuff on that background
+			
+			TextInputFile themeNames = new TextInputFile("themes.txt");
+			int inty = 120;
+			con.setDrawColor(new Color (32,91,110));
+			con.drawString("Choose a theme: ", 40,80);
+			while(themeNames.eof() == false){
+				strTheme = themeNames.readLine();
+				
+				con.drawString(strTheme,40,inty);
+				inty = inty+50;
+			}
+			themeNames.close();
+			strUserChoice = con.readLine();
+			con.drawString(strUserChoice,90,400);
+			//con.repaint();
+			return strUserChoice;
+		}
+		
+		
+		public static boolean QuitControl(Console con){
+			boolean blnQuit = false;
+			char chrQuit = con.getChar();
+			if(chrQuit == 'q' || chrQuit == 'Q'){
+				blnQuit = true;
+				CPTSydneytools.PlayGame(con);
+			}
+			return blnQuit;
 		}
 		
 		public static void PlayGame(Console con){
@@ -27,24 +117,40 @@ public class CPTSydneytools{
 			String strTheme = "";
 			String strWord;
 			String strUserChoice;
+			String strCorrect = "false";
+			int intGuesses = 0;
+			int intCount = 0;
+			int intWinScore = 0;
+			String strUserHint = "";
 			//Getting user's name
-			con.println("Enter User Name: ");
+			strUserName = CPTSydneytools.UserNameInfo(con);
 			//Printing Themes
-			TextInputFile themeNames = new TextInputFile("themes.txt");
-			con.println("Themes: ");
-			while(themeNames.eof() == false){
-				strTheme = themeNames.readLine();
-				con.println(strTheme);
-			}
-			themeNames.close();
+			//TextInputFile themeNames = new TextInputFile("themes.txt");
+			
+			//con.println("Themes: ");
+			//while(themeNames.eof() == false){
+				//strTheme = themeNames.readLine();
+				//con.println(strTheme);
+			//}
+			//themeNames.close();
 			//Using ThemeChosen method to get the theme the user wanted.
-			con.println("Choose a theme: ");
-			strUserChoice = con.readLine();
+			//con.println("Choose a theme: ");
+			//strUserChoice = con.readLine();
+			strUserChoice = CPTSydneytools.Themes(con);
+			
 			strTheme = CPTSydneytools.Themechosen(strUserChoice);
 			strWord = CPTSydneytools.ChooseWord(strTheme);
-			//WORKS THEME & WORD
+			strUserHint = CPTSydneytools.UserGuess(strWord,intGuesses, intWinScore, strCorrect,con);
+			//WORKS THEME & WOD
 			//con.println(strTheme);
 			//con.println(strWord);
+			
+			
+			for(intCount = 0; intCount<= strWord.length(); intCount++){
+				if(strUserHint.equalsIgnoreCase (strWord.substring(intCount, intCount+1))){
+					con.println(strUserHint);
+				}
+			}
 			
 		}
 		
@@ -109,52 +215,51 @@ public class CPTSydneytools{
 		}
 		
 		
-		public static String UserGuess(String strUserChoice){
-			TextInputFile themes = new TextInputFile("themes.txt");
-			String strThemeName = "";
-			int intCount  = 0;
-			//String strThemes [] = new String [5];
-			String strChosenTheme = "";
-			while(themes.eof() == false){
-				strThemeName = themes.readLine();
-				if(strThemeName.equalsIgnoreCase(strUserChoice)){
-					strChosenTheme = strThemeName;
-				}
+		public static String UserGuess(String strWord, int intGuesses, int intWins,String strCorrect, Console con){
+			String strGuess = con.readLine();
+			String strWordSplit [][];
+			int intRand = 0;
+			int intCount = 0;
+			int intCount2 = 0;
+			String strLetterTemp;
+			String strNumTemp;
+			String strUserHint = "";
+			strWordSplit = new String[strWord.length()][2];
+			while(!strWord.equalsIgnoreCase(strGuess)){
+				if (!strGuess.equalsIgnoreCase(strWord)){
+					intGuesses = intGuesses + 1;
+					for (intCount = 0; intCount <= strWord.length(); intCount++){
+						intRand = (int) (Math.random()*100+1);
+						strWordSplit[intCount][0] = strWord.substring(intCount,intCount+1);
+						strWordSplit[intCount][1] = intRand + "";
+					}
+					for(intCount2 = 0; intCount2 < strWord.length()-1; intCount2++){
+						for (intCount = 0; intCount < strWord.length()-1; intCount++){
+							//Convert the people's score to integer and compare
+							if(Integer.parseInt(strWordSplit[intCount][1]) > Integer.parseInt(strWordSplit[intCount+1][1])){
+								strLetterTemp = strWordSplit[intCount][0];
+								strWordSplit[intCount][0] = strWordSplit[intCount+1][0];
+								strWordSplit[intCount+1][0] = strLetterTemp;
+								//swap critics
+								strNumTemp = strWordSplit[intCount][1];
+								strWordSplit[intCount][1] = strWordSplit[intCount+1][1];
+								strWordSplit[intCount+1][1] = strNumTemp;
+						
+							}
+						}
+					}
 				
-				//strThemes[intCount] = strThemeName;
-				//intCount = intCount+1;
+					strUserHint = strWordSplit[intGuesses][0];
+				}else{
+					intWins = intWins+1;
+					strCorrect = "true";
+					return strCorrect;
+				}
 			}
-			
-			//for(intCount = 0; intCount <= intUserChoice; intCount++){
-				//if(intUserChoice == intCount){
-					//strChosenTheme = strThemes[intCount];
-				//}
-			//}
-			
-			return strChosenTheme;
+			return strUserHint;
+	
 		}
 		
-		public static KeyListener QuitonQKeyListener(Console con){
-			return new KeyAdapter(){
-				public void keyPressed(KeyEvent e) {
-					if(e.getKeyCode() == KeyEvent.VK_Q) {
-						con.println("Quitting Game");
-						System.exit(0);
-					}
-				}
-			};
-					
-		}	
-		public static KeyListener PlayonPKeyListener(Console con){
-			return new KeyAdapter(){
-				public void keyPressed(KeyEvent e) {
-					if(e.getKeyCode() == KeyEvent.VK_P) {
-						con.println("Starting Game");
-						
-					}
-				}
-			};
-					
-		}
+
 		
 }
